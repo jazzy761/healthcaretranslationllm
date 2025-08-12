dc97930888c2b3c6503fcc73d40fda558b6b6db5
jahanzaib <jahanzaibqutbi1@gmail.com>
2025-08-12 22:42:32 +0500

    add python-multipart

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from fastapi.responses import HTMLResponse, JSONResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from fastapi.middleware.cors import CORSMiddleware
-from src.llm import LLM
+from llm import LLM
 import gtts
 import os
 
