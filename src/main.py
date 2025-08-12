81c7551a3399a3a27a973be920cd6f506c400e95
jahanzaib <jahanzaibqutbi1@gmail.com>
2025-08-12 22:43:55 +0500

    src.llm

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from fastapi.responses import HTMLResponse, JSONResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from fastapi.middleware.cors import CORSMiddleware
-from llm import LLM
+from src.llm import LLM
 import gtts
 import os
 
